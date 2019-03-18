1d80371fde8da56327764095ce0c80e825256768
Synkronizing <josh6050@gmail.com>
2019-03-18 11:47:01 -0600

    new apps: list, swimming, quadratic, quicklauncher, slope calc

--- JoshSCalculator/app/src/main/java/com/example/joshscalculator/MainActivity.java
+++ JoshSCalculator/app/src/main/java/com/example/joshscalculator/MainActivity.java
@@ -13,58 +13,54 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         Button addBtn = (Button) findViewById(R.id.addBtn);
         Button divBtn = (Button) findViewById(R.id.divBtn);
         Button mulBtn = (Button) findViewById(R.id.mulBtn);
         Button subBtn = (Button) findViewById(R.id.subBtn);
+        Button powBtn = (Button) findViewById(R.id.powBtn);
+        Button cosBtn = (Button) findViewById(R.id.cosBtn);
+        final TextView resultTextView = (TextView) findViewById(R.id.resultTextView);
         addBtn.setOnClickListener(new View.OnClickListener(){
             public void onClick(View view){
-                EditText FirstNumEditText = (EditText) findViewById(R.id.FirstNumEditText);
-                EditText SecondNumEditText = (EditText) findViewById(R.id.SecondNumEditText);
-                TextView resultTextView = (TextView) findViewById(R.id.resultTextView);
-
-                double num1 = Double.parseDouble(FirstNumEditText.getText().toString());
-                double num2 = Double.parseDouble(SecondNumEditText.getText().toString());
-                double result = num1 + num2;
-                resultTextView.setText(result + "");
+                resultTextView.setText((getNumberOne() + getNumberTwo())+ "");
             }
         });
         divBtn.setOnClickListener(new View.OnClickListener(){
             public void onClick(View view){
-                EditText FirstNumEditText = (EditText) findViewById(R.id.FirstNumEditText);
-                EditText SecondNumEditText = (EditText) findViewById(R.id.SecondNumEditText);
-                TextView resultTextView = (TextView) findViewById(R.id.resultTextView);
-
-                double num1 = Double.parseDouble(FirstNumEditText.getText().toString());
-                double num2 = Double.parseDouble(SecondNumEditText.getText().toString());
-                double result = num1 / num2;
-                resultTextView.setText(result + "");
+                resultTextView.setText((getNumberOne() / getNumberTwo())+ "");
             }
         });
         mulBtn.setOnClickListener(new View.OnClickListener(){
             public void onClick(View view){
-                EditText FirstNumEditText = (EditText) findViewById(R.id.FirstNumEditText);
-                EditText SecondNumEditText = (EditText) findViewById(R.id.SecondNumEditText);
-                TextView resultTextView = (TextView) findViewById(R.id.resultTextView);
-
-                double num1 = Double.parseDouble(FirstNumEditText.getText().toString());
-                double num2 = Double.parseDouble(SecondNumEditText.getText().toString());
-                double result = num1 * num2;
-                resultTextView.setText(result + "");
+                resultTextView.setText((getNumberOne() * getNumberTwo())+ "");
             }
         });
         subBtn.setOnClickListener(new View.OnClickListener(){
             public void onClick(View view){
-                EditText FirstNumEditText = (EditText) findViewById(R.id.FirstNumEditText);
-                EditText SecondNumEditText = (EditText) findViewById(R.id.SecondNumEditText);
-                TextView resultTextView = (TextView) findViewById(R.id.resultTextView);
-
-                double num1 = Double.parseDouble(FirstNumEditText.getText().toString());
-                double num2 = Double.parseDouble(SecondNumEditText.getText().toString());
-                double result = num1 - num2;
-                resultTextView.setText(result + "");
+                resultTextView.setText((getNumberOne() - getNumberTwo())+ "");
+            }
+        });
+        powBtn.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                resultTextView.setText(Math.pow(getNumberOne() , getNumberTwo())+ "");
+            }
+        });
+        cosBtn.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                resultTextView.setText(Math.cos(getNumberOne()*getNumberTwo())+ "");
             }
         });
     }
+
+    private double getNumberOne(){
+        EditText FirstNumEditText = (EditText) findViewById(R.id.FirstNumEditText);
+        return  Double.parseDouble(FirstNumEditText.getText().toString());
+
+    };
+    private double getNumberTwo(){
+        EditText SecondNumEditText = (EditText) findViewById(R.id.SecondNumEditText);
+        return  Double.parseDouble(SecondNumEditText.getText().toString());
+
+    };
+
 }
